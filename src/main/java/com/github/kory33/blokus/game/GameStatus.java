4fc40a404ee9e51c4b1ac7890e44dc51fae741c2
Kory33 <konryoitdv@gmail.com>
2017-07-26 08:49:56 +0200

    Add GameStatus Enum / add getOpponentColor method to PlayerColor

---
+++ src/main/java/com/github/kory33/blokus/game/GameStatus.java
@@ -0,0 +1,11 @@
+package com.github.kory33.blokus.game;
+
+/**
+ * Created by Kory on 7/26/2017.
+ */
+public enum GameStatus {
+    RESUME_RED_PLAYER,
+    RESUME_BLUE_PLAYER,
+    GAME_FINISH_RED_WIN,
+    GAME_FINISH_BLUE_WIN;
+}
--- src/main/java/com/github/kory33/blokus/game/cell/PlayerColor.java
+++ src/main/java/com/github/kory33/blokus/game/cell/PlayerColor.java
@@ -4,5 +4,12 @@ package com.github.kory33.blokus.game.cell;
  * Class representing a color of a player.
  */
 public enum PlayerColor {
-    RED, BLUE
+    RED, BLUE;
+
+    public PlayerColor getOpponentColor() {
+        if (this == RED) {
+            return BLUE;
+        }
+        return RED;
+    }
 }
